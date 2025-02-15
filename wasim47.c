804ef1016b8e9d65f8e6911b556af1ade8a69523
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-15 10:26:31 +0530

    Write a C program to swap two numbers witout using third variable and arithmetic operators.

---
+++ wasim47.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter any two numbers\n");
+    scanf("%d%d",&a,&b);
+    a=a^b;
+    b=a^b;
+    a=a^b;
+    printf("A=%d,B=%d",a,b);
+    printf("\n");
+    return 0;
+}
