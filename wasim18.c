d402c60431ecb01ecf7e136b26f20b4f37174686
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-15 09:18:21 +0530

    Write a C program to print square of given number.

---
+++ wasim18.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter any number\n");
+    scanf("%d",&a);
+    printf("Square of given number=%d",a*a);
+    printf("\n");
+    return 0;
+}
