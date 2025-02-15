93ccb99149a9e13fa8a20652391801efc03878df
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-15 10:46:39 +0530

    Write a C program to calculate size of int,flaot,char and double.

---
+++ wasim53.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("Size of int variable=%d\n",sizeof(int));
+    printf("Size of int variable=%d\n",sizeof(float));
+    printf("Size of int variable=%d\n",sizeof(char));
+    printf("Size of int variable=%d\n",sizeof(double));
+    printf("\n");
+    return 0;
+}
