9593f0edb2b064cafdf7de1b95de6d9cb680400d
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-15 09:28:46 +0530

    Write a C program to calculate circumference of the circel.

---
+++ wasim22.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int r;
+    printf("Enter the radius of the circle\n");
+    scanf("%d",&r);
+    printf("Circumference of the circle is=%f",2*3.14*r);
+    printf("\n");
+    return 0;
+}
