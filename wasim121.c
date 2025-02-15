e2a368eb44b649680d77f77f32529db3106b2b40
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-15 15:00:06 +0530

    Write a C program to print 'Galgotias' 5 times.

---
+++ wasim121.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter any number\n");
+    scanf("%d",&n);
+    int i=1;
+    while(i<=n)
+    {
+        printf("Galgotias\n");
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
