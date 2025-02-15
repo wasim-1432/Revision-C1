20febd6f4ec9110340994b2f97bcb0da8fb6d52d
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-15 09:16:35 +0530

    Write a C program to print sum of given two numbers.

---
+++ wasim17.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter any two numbers\n");
+    scanf("%d%d",&a,&b);
+    printf("Sum of given two numbers=%d",a+b);
+    printf("\n");
+    return 0;
+}
