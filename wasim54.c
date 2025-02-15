6a32be10c623f6778cc8323aadb6e60f0bf4f497
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-15 10:50:10 +0530

    Write a C program to make the last digit of a number stored in a variable as zero.

---
+++ wasim54.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter any number\n");
+    scanf("%d",&a);
+    printf("Required answer is=%d",a/10*10);
+    printf("\n");
+    return 0;
+}
