ee4ac613532a3f871bb9f307a372002aa7f07890
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-16 13:09:24 +0530

    Write a C program to print following pattern. ***** *   * *   * *   * *****

---
+++ wasim180.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(j==1 || j==5 || i==1 && j<=5 || i==5 && j<=5)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
