c30f6ba5265a6ba3127a3fca8c73535bfc0b52d8
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-28 18:49:43 +0530

    Write a Menu Driven driven program with the following options. 1)Addition 2)Subtraction 3)Multiplication 4)Division.

---
+++ wasim192.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    float a,b;
+    printf("Enter 1 for Addition\n");
+    printf("Enter 2 for Subtraction\n");
+    printf("Enter 3 for Multiplication\n");
+    printf("Enter 4 for Division\n");
+    scanf("%d",&n);
+    switch(n)
+    {
+        case 1:
+            printf("Enter any two numbers\n");
+            scanf("%d%d",&a,&b);
+            printf("Addition is=%d\n",a+b);
+            break;
+        case 2:
+            printf("Enter any two numbers\n");
+            scanf("%d%d",&a,&b);
+            printf("Subtraction is=%d\n",a-b);
+            break;
+        case 3:
+            printf("Enter any two numbers\n");
+            scanf("%d%d",&a,&b);
+            printf("Multiplication is=%d\n",a*b);
+            break;
+        case 4:
+            printf("Enter any two numbers\n");
+            scanf("%f%f",&a,&b);
+            printf("Division is=%f\n",a/b);
+            break;
+        default:
+            printf("Invalid Input Please Try Again...\n");
+    }
+    printf("\n");
+    return 0;
+}
