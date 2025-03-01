b89f001c5fea7b739449b27267d026c9bf158257
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-03-01 07:46:35 +0530

    Write a C program to print first N odd natural numbers.(Using Function).

---
+++ wasim215.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int Print_First_N_Odd_Natural_Numbers(int n);
+int main()
+{
+    int n;
+    printf("Enter any number\n");
+    scanf("%d",&n);
+    Print_First_N_Odd_Natural_Numbers(n);
+    printf("\n");
+    return 0;
+}
+int Print_First_N_Odd_Natural_Numbers(int n)
+{
+    int i=1;
+    while(i<=n)
+    {
+        printf("%d ",2*i-1);
+        i++;
+    }
+}
