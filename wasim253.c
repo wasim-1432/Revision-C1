83c6ad4853378c3bc3b27f5e20ad6249355da3ac
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-03-04 10:03:23 +0530

    Write a C program to print First N Odd Natural Numbers.(Using Recursion).

---
+++ wasim253.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int Print_First_N_Natural_Odd_Numbers_Using_Recursion(int a);
+int main()
+{
+    int a;
+    printf("Enter any number\n");
+    scanf("%d",&a);
+    Print_First_N_Natural_Odd_Numbers_Using_Recursion(a);
+    printf("\n");
+    return 0;
+}
+int Print_First_N_Natural_Odd_Numbers_Using_Recursion(int a)
+{
+    if(a!=0)
+    {
+        Print_First_N_Natural_Odd_Numbers_Using_Recursion(a-1);
+        printf("%d ",2*a-1);
+    }
+}
