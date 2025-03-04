56c88d99c7c11996e068262cf1c0685cb01a3a6b
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-03-04 12:36:27 +0530

    Write a C program to find the smallest number stored in an array of size 10.Take array values from the user.

---
+++ wasim295.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int arr[10],i;
+    printf("Enter any 10 numbers\n");
+    for(i=0;i<=9;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<=9;i++)
+    {
+        if(arr[i]<arr[i+1])
+        {
+            int temp;
+            temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+    }
+    printf("Min number is=%d",arr[9]);
+    printf("\n");
+    return 0;
+}
