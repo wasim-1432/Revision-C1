79c9ae1c5856c8c74c047ed00be57a6f439f2aeb
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-03-05 08:59:16 +0530

    Write a C program to sort the given string.

---
+++ wasim342.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[30],temp;
+    printf("Enter any string\n");
+    fgets(str,30,stdin);
+    str[strlen(str)-1]='\0';
+    int l,round,i;
+    l=strlen(str);
+    for(round=1;round<=l-1;round++)
+    {
+        for(i=0;i<=l-round-1;i++)
+        {
+            if(str[i]>str[i+1])
+            {
+                temp=str[i];
+                str[i]=str[i+1];
+                str[i+1]=temp;
+            }
+        }
+    }
+    for(i=0;str[i];i++)
+    {
+        printf("%c",str[i]);
+    }
+    printf("\n");
+    return 0;
+}
