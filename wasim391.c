86222bf54b8749024d591b5f09eff3ae33da444c
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-03-07 09:07:16 +0530

    Write a C program to count frequency of each character of the given string using function.

---
+++ wasim391.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<string.h>
+int Count_Frequency_Of_Each_Character(char str[]);
+int main()
+{
+    char str[30];
+    printf("Enter any string\n");
+    fgets(str,30,stdin);
+    str[strlen(str)-1]='\0';
+    Count_Frequency_Of_Each_Character(str);
+    printf("\n");
+    return 0;
+}
+int Count_Frequency_Of_Each_Character(char str[])
+{
+    int i,round,l,count=1;
+    l=strlen(str);
+    for(round=1;round<=l-1;round++)
+    {
+        for(i=0;i<=l-round-1;i++)
+        {
+            if(str[i]>str[i+1])
+            {
+                char temp;
+                temp=str[i];
+                str[i]=str[i+1];
+                str[i+1]=temp;
+            }
+        }
+    }
+    for(i=0;str[i];i++)
+    {
+        if(str[i]==str[i+1])
+        {
+            count++;
+        }
+        else
+        {
+            printf("%c - %d\n",str[i],count);
+            count=1;
+        }
+    }
+}
